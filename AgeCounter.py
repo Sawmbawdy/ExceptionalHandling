b42754bd896c534ff469ea878c4ef632fd34d92f
Sawmbawdy <saiompatro12@gmail.com>
2025-09-12 19:18:33 -0400

    Update AgeCounter.py

--- AgeCounter.py
+++ AgeCounter.py
@@ -1,6 +1,10 @@
 try:
     Age = int(input("Input your age: "))
     print("Your age,",Age, "was successfully stored in our datastores")
+    if Age%2 == 0:
+        print("Number is even")
+    else:
+        print("Number is odd")
 
 except ValueError:
     print("Enter a valid number(s)")
